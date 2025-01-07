b19c20c14440bf7609c58767296456c1dc093354
avni <avniisrani9@gmail.com>
2025-01-07 06:50:51 -0800

    fixed padding on movie image in MovieDetails

--- src/components/MovieDetails.jsx
+++ src/components/MovieDetails.jsx
@@ -7,11 +7,11 @@ function MovieDetails(props) {
   return (
     <div className="flex justify-center items-center fixed inset-0 z-50">
       <div onClick={()=>setMoreDetails(null)} className="flex flex-col items-center justify-center max-w-[50%] max-h-[50%] bg-custom-black p-5 min-w-[400px] min-h-[400px] text-custom-grey outline outline-white rounded-md">
-      <p className="font-bold text-xl pb-2">{movies.results[moreDetails].original_title}</p>
+      <p className="font-bold text-xl pb-2 pt-5">{movies.results[moreDetails].original_title}</p>
         <div className="flex flex-row items-center">
           <img
             src={`https://image.tmdb.org/t/p/w200/${movies.results[moreDetails].poster_path}`}
-            className="rounded-xl w-1/2 h-1/2 m-2"
+            className="rounded-xl max-w-[50%] max-h-[50%] mr-4 mb-4"
           ></img>
           <div className="flex flex-col">
           <p className="pt-2">{movies.results[moreDetails].overview}</p>
